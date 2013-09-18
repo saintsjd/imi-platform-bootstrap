826df3eda1fcd53431c1a7b9d8b68077a6c47f5e
Jon Saints <saintsjd@gmail.com>
2013-09-17 23:20:19 -0600

    deploy

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,25 +1,27 @@
+'use strict';
+
 $(function(){
 
-	$(".filter-btn a").click(function(e){
+	$('.filter-btn a').click(function(){
 		
-		var which_btn = $(this).attr("id").split("-")[0];
-		var already_active = $(this).parent().hasClass("active");
+		var whichBtn = $(this).attr('id').split('-')[0];
+		var alreadyActive = $(this).parent().hasClass('active');
 
-		$(".filter-btn").removeClass("active");
-		$(".filter").slideUp();
+		$('.filter-btn').removeClass('active');
+		$('.filter').slideUp();
 
-		if( !already_active ){
-			$("#" + which_btn + "-filter-btn").parent().addClass("active");
-			$("#" + which_btn + "-filter").slideDown("slow");
+		if( !alreadyActive ){
+			$('#' + whichBtn + '-filter-btn').parent().addClass('active');
+			$('#' + whichBtn + '-filter').slideDown('slow');
 		}
 
 		return false;
 	});
 
-	$(".update-btn").click(function(){
+	$('.update-btn').click(function(){
 
-		$(".filter-btn").removeClass("active");
-		$(".filter").slideUp();
+		$('.filter-btn').removeClass('active');
+		$('.filter').slideUp();
 		return false;
 	});
 
