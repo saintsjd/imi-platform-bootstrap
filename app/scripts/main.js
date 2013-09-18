3003a3fcbbd40ee3c21c0e8aa85f85cd37a0eecd
Jon Saints <saintsjd@gmail.com>
2013-09-17 22:36:37 -0600

    filter bar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,26 @@
-console.log('\'Allo \'Allo!');
+$(function(){
+
+	$(".filter-btn a").click(function(e){
+		
+		var which_btn = $(this).attr("id").split("-")[0];
+		var already_active = $(this).parent().hasClass("active");
+
+		$(".filter-btn").removeClass("active");
+		$(".filter").slideUp();
+
+		if( !already_active ){
+			$("#" + which_btn + "-filter-btn").parent().addClass("active");
+			$("#" + which_btn + "-filter").slideDown("slow");
+		}
+
+		return false;
+	});
+
+	$(".update-btn").click(function(){
+
+		$(".filter-btn").removeClass("active");
+		$(".filter").slideUp();
+		return false;
+	});
+
+});
